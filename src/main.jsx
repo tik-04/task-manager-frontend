48e1268655bcfcfcd14fdb93bcd8dc405cdc7d84
Nattaphan Pumipak <151860004+tik-04@users.noreply.github.com>
2025-03-16 13:57:50 +0700

    add icon for website

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import AppRoutes from "./routes/index";
 import axios from 'axios';
 
+
 const token = localStorage.getItem("token");
 if (token) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 }
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
