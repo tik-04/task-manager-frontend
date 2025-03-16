81c210fd4593fc03382c8defc015a9206c55734f
Nattaphan Pumipak <151860004+tik-04@users.noreply.github.com>
2025-03-17 02:00:26 +0700

    add logout function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../api/axiosInstance";
+
 export default function Navbar() {
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+      localStorage.removeItem("token"); // ✅ ลบ Token ออกจาก localStorage
+      delete axiosInstance.defaults.headers.common["Authorization"]; // ✅ เคลียร์ Header ไม่ให้แนบ Token ไปอีก
+    
+      navigate("/login"); // ✅ Redirect ไปหน้า Login
+    };
+  
     return (
-      <nav className="bg-blue-500 p-4 text-white">
+      <nav className="bg-blue-500 p-4 text-white flex justify-between items-center">
         <h1>My App</h1>
+        <button onClick={() => handleLogout()}>Log out</button>
       </nav>
     );
   }
